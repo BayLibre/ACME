78e58794c55394d86a05b0a37be89fe78a0595c7
Marc Titinger <mtitinger@baylibre.com>
2015-10-26 13:46:56 +0100

    add usb-on-off validation test
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

---
+++ patches/baylibre-acme/fs-overlay/root/test-sigrok.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$1" == "" ]
+then
+ echo "usage: $0 file-name (will be a csv file)"
+ exit 1
+fi
+
+sigrok-cli  -d baylibre-acme --samples 2000 --config samplerate=500 -O csv  > ../$1.csv &
+echo 0 > /sys/class/gpio/gpio489/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio489/value
+
+
