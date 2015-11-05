e6b046923545564d159ce9c402e4fc3e00b65470
Marc Titinger <mtitinger@baylibre.com>
2015-11-05 16:42:29 +0100

    checkout sigrok-cli from a generic-iio branch
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- scripts/sigrok/makelinks.sh
+++ scripts/sigrok/makelinks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd $ACME_HOME/buildroot/output/build
 mv libsigrok-generic-iio libsigrok-generic-iio_OLD
-mv sigrok-cli-master sigrok-cli-master_OLD
+mv sigrok-cli-generic-iio sigrok-cli-generic-iio_OLD
 
 ln -s ../../../libsigrok libsigrok-generic-iio
-ln -s ../../../sigrok-cli sigrok-cli-master
+ln -s ../../../sigrok-cli sigrok-cli-generic-iio
 
