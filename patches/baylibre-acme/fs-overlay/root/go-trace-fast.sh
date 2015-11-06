52cda8d512e4c11943d5de076d21cd7677fd27d9
Marc Titinger <mtitinger@baylibre.com>
2015-11-06 16:58:13 +0100

    update
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- patches/baylibre-acme/fs-overlay/root/go-trace-fast.sh
+++ patches/baylibre-acme/fs-overlay/root/go-trace-fast.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 echo 0 > /sys/bus/iio/devices/iio\:device0/buffer/enable
 
-echo 1024 > /sys/bus/iio/devices/iio\:device0/buffer/enable
+echo 1024 > /sys/bus/iio/devices/iio\:device0/buffer/length
 echo 1 > /sys/bus/iio/devices/iio\:device0/in_mean_raw
-
 
 echo 1 >  /sys/bus/iio/devices/iio:device0/scan_elements/in_power3_en
 echo 1  > /sys/bus/iio/devices/iio:device0/scan_elements/in_timestamp_en
 echo 2000  > /sys/bus/iio/devices/iio:device0/in_sampling_frequency
 
 trace-cmd start -p nop
-iio_readdev ina226 > result &
-sleep 2
-tree /etc
-sleep 2
-ls -al
-sleep 2
-zcat /proc/config.gz | grep PM > /dev/null
-sleep 4
+
+echo 1 > /sys/bus/iio/devices/iio\:device0/buffer/enable
+
+dd if=/dev/iio\:device0 of=/result bs=16k count=5000
 
 trace-cmd stop
 trace-cmd extract
