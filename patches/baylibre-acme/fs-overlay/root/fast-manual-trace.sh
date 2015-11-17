4666d1db9c3e77472a0add654f4373551385f18d
Marc Titinger <mtitinger@baylibre.com>
2015-11-17 12:00:20 +0100

    update patches folder
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- patches/baylibre-acme/fs-overlay/root/fast-manual-trace.sh
+++ patches/baylibre-acme/fs-overlay/root/fast-manual-trace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
 
-FREQ=455
+FREQ=850
 
 ./swtrigger-setup.sh $FREQ
 
@@ -10,17 +10,18 @@ echo 0 > /sys/bus/iio/devices/iio\:device0/buffer/enable
 
 echo 1024 > /sys/bus/iio/devices/iio\:device0/buffer/length
 
-echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_voltage0_en
-echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_voltage1_en
-echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_current2_en
 echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_power3_en
 echo 1 > /sys/bus/iio/devices/iio:device0/scan_elements/in_timestamp_en
+
+echo 0 > /sys/bus/iio/devices/iio:device0/scan_elements/in_current2_en
+echo 0 > /sys/bus/iio/devices/iio:device0/scan_elements/in_voltage0_en
+echo 0 > /sys/bus/iio/devices/iio:device0/scan_elements/in_voltage1_en
 
 echo test1 >  /sys/bus/iio/devices/iio\:device0/trigger/current_trigger
 
 trace-cmd start -p nop
 
-echo 4 > /sys/bus/iio/devices/iio\:device0/in_averaging_steps
+echo 1 > /sys/bus/iio/devices/iio\:device0/in_averaging_steps
 echo $FREQ > /sys/bus/iio/devices/iio:device0/in_sampling_frequency
 
 echo 1 > /sys/bus/iio/devices/iio\:device0/buffer/enable
--- patches/baylibre-acme/fs-overlay/root/swtrigger-setup.sh
+++ patches/baylibre-acme/fs-overlay/root/swtrigger-setup.sh
@@ -4,19 +4,12 @@
 modprobe industrialio-sw-trigger
 modprobe iio-trig-hrtimer
 
-lsmod | grep industrialio
-
 # mount configfs
 mount -t configfs none /config
-
-#check for the iio/trigger folder
-find /config
-
-sleep 1
 
 mkdir /config/iio/triggers/hrtimer/test1
 
 find /sys/bus/iio/devices/trigger0/
 
-echo 1500 > /sys/bus/iio/devices/trigger0/sampling_frequency
+echo $1 > /sys/bus/iio/devices/trigger0/sampling_frequency
 
