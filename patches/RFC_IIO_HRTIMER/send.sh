4183bd06821ab1646a9659556404e6d3e20ba909
Marc Titinger <mtitinger@baylibre.com>
2015-11-18 16:35:18 +0100

    patches: add HRTIMER related series
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

---
+++ patches/RFC_IIO_HRTIMER/send.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git send-email --to jic23@kernel.org --to knaack.h@gmx.de --to lars@metafoo.de \
+--to pmeerw@pmeerw.net --cc daniel.baluta@intel.com \
+--cc linux-kernel@vger.kernel.org --cc linux-iio@vger.kernel.org \
+--no-chain-reply-to 000*
+
