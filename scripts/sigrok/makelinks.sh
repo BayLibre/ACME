2e253c70c04a429cb56c8133aa88ebde929513e6
Marc Titinger <mtitinger@baylibre.com>
2015-12-23 17:37:35 +0100

    simplify script folder
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- scripts/sigrok/makelinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $ACME_HOME/buildroot/output/build
-mv libsigrok-generic-iio libsigrok-generic-iio_OLD
-mv sigrok-cli-generic-iio sigrok-cli-generic-iio_OLD
-
-ln -s ../../../libsigrok libsigrok-generic-iio
-ln -s ../../../sigrok-cli sigrok-cli-generic-iio
-
